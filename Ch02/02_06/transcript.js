5ee71d13352c8cf59af56b7f78e830042c87f9f4
VAMAN ACHARYA <vaman.acharya001@gmail.com>
2024-01-01 15:52:11 +0530

    Update transcript.js

--- Ch02/02_06/transcript.js
+++ Ch02/02_06/transcript.js
@@ -1,6 +1,6 @@
 // This is a transcript of the lines of code from the lesson.
 // You can copy and paste each line into a JavaScript console to execute it and see the result.
-
+// if you do not want to change value of the variable at any point of time use "const"
 const dozen = 12, halfDozen = 6, bakersDozen = 13;
 
 dozen = 13;
