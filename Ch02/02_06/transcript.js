a0a6f6f1040ddadfa4aa8425d350fff59868e372
VAMAN ACHARYA <vaman.acharya001@gmail.com>
2024-01-01 15:54:24 +0530

    Update transcript.js

--- Ch02/02_06/transcript.js
+++ Ch02/02_06/transcript.js
@@ -1,11 +1,14 @@
 // This is a transcript of the lines of code from the lesson.
 // You can copy and paste each line into a JavaScript console to execute it and see the result.
 // if you do not want to change value of the variable at any point of time use "const"
+// const are used for immutable property
 const dozen = 12, halfDozen = 6, bakersDozen = 13;
 
 dozen = 13;
 
+// mutable property
 var cookieCount = 5;
+// mutable property
 let cookieCount = 5;
 
 // More info:
